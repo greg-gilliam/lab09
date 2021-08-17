14f5225e854abacf2887aa34989f1ad47ed346df
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-08-17 16:40:47 -0700

    pushing

--- src/DessertDetail.js
+++ src/DessertDetail.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
+import { getDessert, getTypes } from './fetch-utils.js';
 
 class DessertDetail extends Component {
-    state = {  };
+    state = { 
+        id: 0,
+        name: '',
+        icing: '',
+        type_id: 0,
+     };
+
+     componentDidMount = async () => {
+         const dessertId = this.props.match.params.id;
+         const dessertData = await getDessert(dessertId);
+         const types = await getTypes();
+         this.setState({ ...dessertData, types });
+     };
     render() { 
-        return (<h1>Dessert Detail</h1>);
-    }
+        return (
+        <>
+            <h1>{this.state.name}</h1>
+            <form id="update-dessert">
+                <div className="form-group">
+                    <label>Dessert: </label>
+                    <input 
+                        onChange={(e) => {
+                            this.setState({ name: e.target.value });
+                        }}
+                        type="text"
+                        value={this.state.name}></input>
+                </div>
+                <div className="form-group">
+                    <label>Type: </label>
+                    <select>
+                        {this.state.types.map((type) => {
+                            return (
+                                <option value={type.name}>{type.name}</option>
+                            );
+                        })}
+                    </select>
+                </div>
+            </form>
+        </>
+    );
 }
- 
-export default DessertDetail
+}
+
+export default DessertDetail;
--- src/DessertList.js
+++ src/DessertList.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import { getDesserts } from './fetch-utils.js';
+// import { Link } from 'react-router-dom';
 
 class DessertList extends Component {
-    state = {  }
-    render() { 
-        return (<h1>Dessert List</h1>);
+    state = { desserts: [] };
+    componentDidMount = async () => {
+        const data = await getDesserts();
+        this.setState({ cartoons: data });
+    };
+     render() { 
+        return (
+            <section className="dessert-list">
+                {this.state.desserts.map((c) => (
+                    <div className="dessert-card">
+                        <h2>
+                            {/* <Link to={`/cartoons/${dessert.id}`}>{dessert.name}</Link> */}
+                        </h2>
+                    </div>
+                ))}
+            </section>
+        );
     }
 }
  
