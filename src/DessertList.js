096c71907ff1752286afedb107aad5398351cd69
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-08-26 16:42:51 -0700

    fixed

--- src/DessertList.js
+++ src/DessertList.js
@@ -14,7 +14,7 @@ class DessertList extends Component {
                 {this.state.desserts.map((c) => (
                     <div className="dessert-card">
                         <h2>
-                            <Link key={`/desserts`} to={`/desserts`}>{c.name}</Link>
+                            <Link key={`/desserts`} to={`/desserts/${c.id}`}>{c.name}</Link>
                         </h2>
                     </div>
                 ))}
